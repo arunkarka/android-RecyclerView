b8398861bfee7a64e5c3b24ea4fbcf9637ff789c
arunkarka <arunkarka@gmail.com>
2018-12-25 14:47:35 -0600

    Removed unnecessary views and logging stuff

--- Application/src/main/java/com/example/android/common/activities/SampleActivityBase.java
+++ Application/src/main/java/com/example/android/common/activities/SampleActivityBase.java
@@ -34,19 +34,5 @@ public class SampleActivityBase extends FragmentActivity {
         super.onCreate(savedInstanceState);
     }
 
-    @Override
-    protected  void onStart() {
-        super.onStart();
-        initializeLogging();
-    }
 
-    /** Set up targets to receive log data */
-    public void initializeLogging() {
-        // Using Log, front-end to the logging chain, emulates android.util.log method signatures.
-        // Wraps Android's native log framework
-        LogWrapper logWrapper = new LogWrapper();
-        Log.setLogNode(logWrapper);
-
-        Log.i(TAG, "Ready");
-    }
 }
